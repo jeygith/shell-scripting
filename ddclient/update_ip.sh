85e633bcccb7ce9ae13e6e68489fc5991b5a44b2
Jeff Githiri <githireh@gmail.com>
2020-03-27 21:12:09 +0300

    Add raddar and refactor the domains
     - Add radarr domain name to list of domains
    
     - Refactor the domain loop and remove `.githire-svr` from the list of
    domains to be added dynamically in the for loop

--- ddclient/update_ip.sh
+++ ddclient/update_ip.sh
@@ -11,15 +11,24 @@ declare wanip4=$(dig @resolver1.opendns.com A myip.opendns.com +short -4)
 declare secret=3Ef22XoBTj3AnQE1
 
 declare -a domains=(githire-svr transmission
-                    transmission.githire-svr pihole.githire-svr plex.githire-svr
-                    headphones.githire-svr tautulli.githire-svr sonarr.githire-svr
-                    couchpotato.githire-svr jackett.githire-svr
+                    pihole plex headphones tautulli sonarr
+                    couchpotato jackett radarr
                     )
 
 for domain in "${domains[@]}"; do
+
+# check if domain is tld githire-svr and not append the .githire-svr in the domain name
+  if [ "${domain}" != "githire-svr" ]
+  then
+    domain="${domain}.githire-svr"
+    echo "${domain}"
+  fi
+
+  #construct the url
   declare url="http://64.227.45.220:8080/update?secret=${secret}&domain=${domain}&addr=${wanip4}"
 
   echo ${url}
 
+# make curl call to update the ip for the various domains
   curl -s "${url}"
 done

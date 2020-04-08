d338e0202cba103c8528cbb6a96eac0edd7f0a47
Jeff Githiri <githireh@gmail.com>
2020-04-08 15:07:09 +0300

    Add ombi and heimdall

--- ddclient/update_ip.sh
+++ ddclient/update_ip.sh
@@ -13,6 +13,7 @@ declare secret=3Ef22XoBTj3AnQE1
 declare -a domains=(githire-svr transmission
                     pihole plex headphones tautulli sonarr
                     couchpotato jackett radarr lidarr unms lazylibrarian
+                    ombi heimdall
                     )
 
 for domain in "${domains[@]}"; do

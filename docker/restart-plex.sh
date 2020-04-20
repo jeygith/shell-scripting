ea8a9e0ed199cb7ab3d8834dcb771826afddf92e
Jeff Githiri <githireh@gmail.com>
2020-04-20 10:02:30 +0300

    Add docker-plex restart script

---
+++ docker/restart-plex.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#this script gets the ipv4 and updates to custom ddns server
+
+DATE=`date '+%Y-%m-%d %H:%M:%S'`
+echo "Plex docker restart at: ${DATE}"
+
+
+cd $HOME/docker/plex && /usr/local/bin/docker-compose down && /usr/local/bin/docker-compose up -d && echo "docker-plex restart at: ${DATE}"

38f69586174a3d49e5ff9a0cd2c56556e9aa9e14
githire <githireh@gmail.com>
2020-05-26 23:11:52 +0300

    Modify certbot.sh

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -1,4 +1,11 @@
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-/usr/bin/certbot  -a \! -d /run/systemd/system &&  certbot -q renew
+#/usr/local/bin/certbot-auto  -a \! -d /run/systemd/system &&  certbot -q renew
+
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+echo "Certbot renew run at: ${DATE}"
+
+
+python3 -c 'from time import gmtime, strftime; strftime("%Y-%m-%d %H:%M:%S", gmtime()); import random; import time; time.sleep(random.random() * 10)' && /usr/local/bin/certbot-auto renew 2>&1
+

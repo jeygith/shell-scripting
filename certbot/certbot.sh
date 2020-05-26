6bc338a6f9ba8a1493fb0d10550cc0179b290399
Jeff Githiri <githireh@gmail.com>
2020-05-26 23:24:12 +0300

    sleep for 15mins

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -7,5 +7,5 @@ DATE=$(date '+%Y-%m-%d %H:%M:%S')
 echo "Certbot renew run at: ${DATE}"
 
 
-python3 -c 'from time import gmtime, strftime; strftime("%Y-%m-%d %H:%M:%S", gmtime()); import random; import time; time.sleep(random.random() * 10)' && /usr/local/bin/certbot-auto renew 2>&1
+python3 -c 'from time import gmtime, strftime; strftime("%Y-%m-%d %H:%M:%S", gmtime()); import random; import time; time.sleep(random.random() * 900)' && /usr/local/bin/certbot-auto renew 2>&1
 

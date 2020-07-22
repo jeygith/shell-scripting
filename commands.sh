ebbcbae38d4ec61f27717685ee955ae7ad52ffdf
githire <githire@gmail.com>
2020-07-22 19:07:27 +0300

    Add hass domain

--- commands.sh
+++ commands.sh
@@ -8,7 +8,7 @@ spotify-ripper music/playlists/"Best of Khaligraph Jones.txt" -u githireh@gmail.
 
 spotify-ripper music/playlists/"Lucky you.txt" -u githireh@gmail.com --flac -u githireh@gmail.com -p "_JF)GTWjMddYw35"
 
-spotify-ripper music/playlists/"TheBaddest"
+spotify-ripper music/playlists/"unleashed"
 
 spotify-ripper music/playlists/"streetbullies.txt"  -f "{artist} - {track_name}.{ext}" --directory "/media/2TB/software/spotify-ripper/music/riddim/Street Bullies Riddim"
 
@@ -22,5 +22,4 @@ docker run --rm -it -v $(pwd)/Hanyaring\ Game:/music ritiek/spotify-downloader -
 
 docker run --rm -it -v $(pwd)/"music/playlists":/music ritiek/spotify-downloader -p https://open.spotify.com/playlist/5zqUzVRqn6NZxAEeYkl6SN
 
-docker run --rm -it -v $(pwd)/"music/playlists":/music ritiek/spotify-downloader -p https://open.spotify.com/playlist/7CkCllQ3Zklt8yBW7oPYSC
-
+docker run --rm -it -v $(pwd)/"music/playlists":/music ritiek/spotify-downloader -p https://open.spotify.com/playlist/3lvow4CJMct5ApgxcqhHae

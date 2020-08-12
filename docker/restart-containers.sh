fcf875b57057d273f19129543ba673a290418ece
Jeff Githiri <githireh@gmail.com>
2020-08-12 13:13:34 +0300

    Add script to restart docker-containers

---
+++ docker/restart-containers.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+declare -a containers=(calibre
+  calibre-web
+  docker-couchpotato
+  docker-headphones
+  docker-jackett
+  headphones
+  heimdall
+  home-assistant
+  jackett
+  lazylibrarian
+  librenms
+  lidarr
+  mosquitto
+  nodered
+  ombi
+  pihole
+  plex
+  radarr
+  sonarr
+  spotify-docker-ripper
+  spotify-downloader
+  tautulli
+  transmission
+  ubooquity
+  varken
+)
+
+for container in "${containers[@]}"; do
+  cd "/home/githire/docker/${container}" && docker-restart
+done

16b9300e248760f73a2fbdbb8675b985b24e1fac
Jeff Githiri <githireh@gmail.com>
2020-08-12 13:40:31 +0300

    change to docker-compose up onlu

--- docker/restart-containers.sh
+++ docker/restart-containers.sh
@@ -28,5 +28,5 @@ declare -a containers=(calibre
 )
 
 for container in "${containers[@]}"; do
-  cd "/home/githire/docker/${container}" && docker-compose down && docker-compose up -d
+  cd "/home/githire/docker/${container}" && docker-compose up -d
 done

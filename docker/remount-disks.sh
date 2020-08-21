d02f360762e6e046ab44f2b4a678fb490def3ede
githire <githire@gmail.com>
2020-08-21 13:14:02 +0300

    Add exit code

--- docker/remount-disks.sh
+++ docker/remount-disks.sh
@@ -6,12 +6,15 @@ echo "Remount disks at: ${DATE}"
 
 #sudo echo "me"
 
+# Test if disks are un mounted
 ls -la /media/openworld
 
 if [ $? -eq 0 ]; then
   echo OK
+  exit 0
 else
   echo FAIL
+  exit 1
 fi
 
 #sudo umount -l /media/* && sudo systemctl restart autofs && cd /home/githire/shell-scripting/docker && ./restart-select-containers.sh

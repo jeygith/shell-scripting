88b50f57f79a3efdaa5ea53209de47bcc0e3571c
githire <githire@gmail.com>
2020-09-05 13:45:44 +0300

    Full path ip

--- ip/pi-hole-static.sh
+++ ip/pi-hole-static.sh
@@ -9,7 +9,7 @@ EXIST=$(/usr/sbin/ip route show 10.0.0.192/27 | wc -l)
 
 if [ $EXIST -eq 0 ]; then
   echo "pihole route does not exist"
-  ip link add foobar link enp2s0 type macvlan mode bridge && ip addr add 10.0.0.199/32 dev foobar && ip link set foobar up && ip route add 10.0.0.192/27 dev foobar
+  /usr/sbin/ip link add foobar link enp2s0 type macvlan mode bridge && /usr/sbin/ip addr add 10.0.0.199/32 dev foobar && /usr/sbin/ip link set foobar up && /usr/sbin/ip route add 10.0.0.192/27 dev foobar
   echo "route added"
 fi
 if [ $EXIST -eq 1 ]; then

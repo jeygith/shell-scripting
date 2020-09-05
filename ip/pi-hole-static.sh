5463f98ac64ef0984bb17bb58ffa51ae3bfd8566
githire <githire@gmail.com>
2020-09-05 12:15:57 +0300

    Mod route

--- ip/pi-hole-static.sh
+++ ip/pi-hole-static.sh
@@ -5,12 +5,19 @@
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 echo "Create static ip route at: ${DATE}"
 
-ip link add foobar link enp2s0 type macvlan mode bridge && ip addr add 10.0.0.199/32 dev foobar && ip link set foobar up && ip route add 10.0.0.192/27 dev foobar
+piholeroute=$(route | grep "10.0.0.192")
 
-if [ $? -eq 0 ]; then
-  echo "static route created ok"
+if [ -z "$piholeroute" ]; then
+  echo "pihole route exits"
   exit 0
 else
-  echo "Error creating static route"
-  exit 1
+  ip link add foobar link enp2s0 type macvlan mode bridge && ip addr add 10.0.0.199/32 dev foobar && ip link set foobar up && ip route add 10.0.0.192/27 dev foobar
+
+  if [ $? -eq 0 ]; then
+    echo "static route created ok"
+    exit 0
+  else
+    echo "Error creating static route"
+    exit 1
+  fi
 fi

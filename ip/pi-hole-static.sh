a76138d012e37eb0e320ae1525d19722cbcd6f2d
githire <githire@gmail.com>
2020-09-05 13:31:05 +0300

    Refactor route check

--- ip/pi-hole-static.sh
+++ ip/pi-hole-static.sh
@@ -5,14 +5,13 @@
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 echo "Create static ip route at: ${DATE}"
 
-if route | grep "10.0.0.192"; then
-  echo "pihole route exits"
-else
-  ip link add foobar link enp2s0 type macvlan mode bridge && ip addr add 10.0.0.199/32 dev foobar && ip link set foobar up && ip route add 10.0.0.192/27 dev foobar
+EXIST=$(ip route show 10.0.0.192/27 | wc -l)
 
-  if [ $? -eq 0 ]; then
-    echo "static route created ok"
-  else
-    echo "Error creating static route"
-  fi
+if [ $EXIST -eq 0 ]; then
+  echo "pihole route does not exist"
+  ip link add foobar link enp2s0 type macvlan mode bridge && ip addr add 10.0.0.199/32 dev foobar && ip link set foobar up && ip route add 10.0.0.192/27 dev foobar
+  echo "route added"
+fi
+if [ $EXIST -eq 1 ]; then
+    echo "pihole route exits"
 fi

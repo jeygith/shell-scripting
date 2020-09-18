3e0bba9874349ebd13b52b5c8c0cb3a9771106eb
Jeff Githiri <githireh@gmail.com>
2020-09-18 21:42:21 +0300

    change calibre-svr to calibre-web

--- ddclient/update_ip.sh
+++ ddclient/update_ip.sh
@@ -33,7 +33,7 @@ if [[ $wanip4 =~ $ip4 ]] && [[ $currentIP =~ $ip4 ]] && [[ "$wanip4" != "$curren
   declare -a domains=(githire-svr transmission
     pihole plex headphones tautulli sonarr
     couchpotato jackett radarr lidarr unms lazylibrarian
-    ombi heimdall calibre calibre-svr preview hass nodered erx router
+    ombi heimdall calibre calibre-web preview hass nodered erx router
     librenms varken mqtt openvpn rancher codeserver cockpit grocy
     healthchecks wireshark sqlitebrowser
   )

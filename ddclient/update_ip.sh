c0ceda7b851db83e71921beadb426a70b1c93633
Jeff Githiri <githireh@gmail.com>
2020-09-18 21:37:42 +0300

    Add grocy

--- ddclient/update_ip.sh
+++ ddclient/update_ip.sh
@@ -34,7 +34,8 @@ if [[ $wanip4 =~ $ip4 ]] && [[ $currentIP =~ $ip4 ]] && [[ "$wanip4" != "$curren
     pihole plex headphones tautulli sonarr
     couchpotato jackett radarr lidarr unms lazylibrarian
     ombi heimdall calibre calibre-svr preview hass nodered erx router
-    librenms varken mqtt openvpn rancher codeserver cockpit
+    librenms varken mqtt openvpn rancher codeserver cockpit grocy
+    healthchecks wireshark sqlitebrowser
   )
 
   for domain in "${domains[@]}"; do

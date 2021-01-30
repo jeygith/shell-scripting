48534bc6ac1c821f57a147ddc6a1f53581891bbc
Jeff Githiri <githireh@gmail.com>
2021-01-30 09:46:36 +0300

    add start containers

--- docker/docker-prune.sh
+++ docker/docker-prune.sh
@@ -4,4 +4,4 @@
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
 echo "docker prune at: ${DATE}"
 
-/home/githire/shell-scripting/docker/start-containers.sh && /usr/bin/docker system prune -a --volumes -f
+/home/githire/shell-scripting/docker/start-containers.sh && /usr/bin/docker system prune -a --volumes -f && /home/githire/shell-scripting/docker/start-containers.sh
